31de9e4ab4c433f303a7b1268fb1ddd5cb70cbb7
Chris <andre89gomez@gmail.com>
2024-01-03 13:07:20 -0800

    Fontawesome added to App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 // import { io } from 'socket.io-client';
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
