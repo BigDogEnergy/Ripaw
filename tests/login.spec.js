46a677bacc3abd82ccd6795be4f2737d2f681c57
Chris <andre89gomez@gmail.com>
2024-03-20 20:22:55 -0700

    Fixed testing error

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,24 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-// test.use({ storageState: 'playwright/.auth/user.json' });
+test.use({ storageState: 'playwright/.auth/user.json' });
 
-// test('Logged-In Test', async ({ page }) => {
-//     await page.goto('https://ripbawbanking.onrender.com/');
-//     await page.context().storageState({ path: 'playwright/.auth/user.json' });
-//     await expect(page.getByRole('link', { name: 'Accounts Account Details' })).toBeVisible();
-//     await expect(page.getByRole('link', { name: 'Transactions Transactions' })).toBeVisible();
-//     await expect(page.getByRole('link', { name: 'Messaging Messaging' })).toBeVisible();
-// });
-
-const userFile = 'playwright/.auth/user.json';
-
-test.describe(() => {
-    test.use({ storageState: userFile });
-    test('Logged-In Test', async ({ page }) => {
-        await page.goto('https://ripbawbanking.onrender.com/');
-        await expect(page.getByRole('link', { name: 'Accounts Account Details' })).toBeVisible();
-        await expect(page.getByRole('link', { name: 'Transactions Transactions' })).toBeVisible();
-        await expect(page.getByRole('link', { name: 'Messaging Messaging' })).toBeVisible();
-    });
+test('Login Functionality Test', async ({ page }) => {
+    await page.goto('https://ripbawbanking.onrender.com/');
+    await page.getByRole('button', { name: 'Log In' }).click();
+    await page.getByLabel('Email').click();
+    await page.getByLabel('Email').fill('demo@aa.io');
+    await page.getByLabel('Email').press('Tab');
+    await page.getByLabel('Password').fill('password');
+    await page.locator('#modal-content').getByRole('button', { name: 'Log In' }).click();
+    await expect(page.getByRole('link', { name: 'Accounts Account Details' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Transactions Transactions' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Messaging Messaging' })).toBeVisible();
 });
-
