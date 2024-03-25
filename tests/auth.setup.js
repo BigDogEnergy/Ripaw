9c90304e001b7fcb88f785dafc10b3f87e3595ce
Chris <andre89gomez@gmail.com>
2024-03-25 15:24:27 -0700

    Updates

--- tests/auth.setup.js
+++ tests/auth.setup.js
@@ -1,16 +1,16 @@
-import { test as setup } from '@playwright/test';
+// import { test as setup } from '@playwright/test';
 
-// Currently getting the user.json but we are unable to properly use it for validation.
-// Come back later to try again.
+// // Currently getting the user.json but we are unable to properly use it for validation.
+// // Come back later to try again.
 
-const userFile = 'playwright/.auth/user.json';
+// const userFile = 'playwright/.auth/user.json';
 
-setup('Authenticate - Set storageState user.json', async ({ request }) => {
-  await request.post('https://ripbawbanking.onrender.com/login', {
-    form: {
-      'user': 'demo@aa.io',
-      'password': 'password'
-    }
-  });
-  await request.storageState({ path: userFile });
-});
+// setup('Authenticate - Set storageState user.json', async ({ request }) => {
+//   await request.post('https://ripbawbanking.onrender.com/login', {
+//     form: {
+//       'user': 'demo@aa.io',
+//       'password': 'password'
+//     }
+//   });
+//   await request.storageState({ path: userFile });
+// });
